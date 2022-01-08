0f8b3f05db4fd4efaa39280917a559b4b02a73c4
alona <alenalogvinova20134@gmail.com>
2022-01-08 12:36:04 +0200

    .

--- lesson6/index.php
+++ lesson6/index.php
@@ -8,33 +8,92 @@
 //
 //2. Продолжение задачи 1. Если записей в $_COOKIE['submissions'] больше 10, перезаписывать старые отправки."
 
-$NameErr = $ageErr = "";
-$ame = $Email = $age = "";
-$lastName = test_input($_POST["lastName"]);
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["firstName"])) {
-        $firstNameErr = "First name is required";
-    } else {
-        $firstName = ($_POST["firstName"]);
-    }
 
-    if (empty($_POST["age"])) {
-        $ageErr = "Age is required";
-    } else {
-        $age = test_input($_POST["age"]);
-    }
-}
+//$userName = $userEmail = $userMessage = "";
+//$errorMsg = "";
+//if (isset($_POST['click'])) {
+//    setcookie('name', 'value', 0,'/');
+//    header('Location: http://localhost');
+//    exit;
+//    print("Name: " . $_POST['name']);
+//    print("<br>Email: " . $_POST['email']);
+//    print("<br>Сообщение: " . $_POST['message']);
+//}
+?>
 
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Forma</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <style ="text/css">
+            table{
+                border:1px solid black;
+                margin-top:30px
+            }
+            td,tr{
+                border:1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+    <form action="index.php" method="POST" name="Form">
+        <div class="mb-3">
+            <label for="Name" class="form-label">Name</label>
+            <input type="text" name="name" class="form-control" id="Name" required="required">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+            <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
 
-echo "<h2>Приятно познакомиться:</h2>";
-echo $firstName;
-echo "<br>";
-echo $lastName;
-echo "<br>";
-echo $age;
+        <button type="submit" name='click' class="btn btn-primary">Send</button>
+
+        <script>
+            function addNewRow(val1, val2, val3){
+                const tbl = document.getElementById('Table');
+                const tblValue = tbl.innerHTML;
+                tbl.innerHTML = tblValue + '<td><td>'+val1+'</td><td>'+val2+'</td></td>'+val3+'</td></tr>';
+            }
+        </script>
+
+    </form>
+    <?php
+    $html="<table>";
+    $html.="<tr>";
+    $html.="<td>";
+    $html.=$_POST['name'];
+    $html.="</td>";
+    $html.="<td>";
+    $html.=$_POST['email'];
+    $html.="</td>";
+    $html.="<td>";
+    $html.=$_POST['message'];
+    $html.="</td>";
+    $html.="</tr>";
+    $html.="</table>";
+    echo $html;
+    ?>
+    </body>
+    </html>
+
+
+<?php
+//setcookie("name", $value, time()+3600);
+//setcookie("email", $value, time()+3600);
+//setcookie("message", $value, time()+3600);
+//print_r($_COOKIE);
+
+//if ($_POST) {
+//    echo '<pre>';
+//    echo htmlspecialchars(print_r($_POST, true));
+//    echo '</pre>';
+//}
+
+?>
+
+
