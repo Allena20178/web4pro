882d9e3180b0ecf9041910203d84c4370caa92e2
alona <alenalogvinova20134@gmail.com>
2022-01-15 00:24:22 +0200

    .

--- lesson6/index.php
+++ lesson6/index.php
@@ -11,90 +11,80 @@
 
 ?>
 
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Forma</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <style ="text/css">
-            table{
-                border:1px solid black;
-                margin-top:30px
-            }
-            td,tr{
-                border:1px solid black;
-            }
-        </style>
-    </head>
-    <body>
-    <form action="index.php" method="POST" name="Form">
-        <div class="mb-3">
-            <label for="Name" class="form-label">Name</label>
-            <input type="text" name="name" class="form-control" id="Name" required="required">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email</label>
-            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Message</label>
-            <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Forma</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style ="text/css">
+    body {
+        margin: 5%;
+    }
+    table{
+        border:1px solid black;
+        margin-top:30px
+    }
+    td,tr{
+        border:1px solid black;
+        padding: 10px;
+    }
+    </style>
+</head>
 
-        <button type="submit" name='click' class="btn btn-primary">Send</button>
+<body>
+<form action="" method="POST" name="Form">
+    <div class="mb-3">
+        <label for="Name" class="form-label">Name</label>
+        <input type="text" name="name" class="form-control" id="Name" required="required">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email</label>
+        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+        <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
 
+    <button type="submit" name='click' class="btn btn-primary">Send</button>
+</form>
 
+<?php
+if (!isset($_COOKIE['submissions'])) $_COOKIE['submissions'] = "[]";
+if (!isset($_COOKIE['position'])) $_COOKIE['position'] = 0;
 
-    </form>
-    <?php
+$data = json_decode($_COOKIE['submissions'], true);
+$position = json_decode($_COOKIE['position'], true);
+
+if (!empty($_POST)) {
 
     $new_values = array(
         'name' => $_POST["name"],
         'email' => $_POST["email"],
         'message' => $_POST["message"]
     );
+    unset($_POST);
 
-$data = json_decode($_COOKIE['submissions'], true);
+    $data[$position] = $new_values;
+    $position = $position + 1;
+    if ($position >= 10) $position = 0;
 
-if($data[10] == null) {
-    $old_index = json_decode($_COOKIE['old_submission_index'], true);
-
-    if ($old_index) {
-        $old_index = 0;
-
-    } else {
-        $old_index = $old_index + 1;
-
-        setcookie('old_submission_index', json_encode($old_index), time() + 3600);
-    }
-
-    $data[$old_index] = $new_values;
-
-    $values = $data;
+    setcookie('position', $position, time()+3600);
+    setcookie('submissions', json_encode($data), time()+3600);
 }
-//    var_dump($data);
-//} else {
-//
-//    $values = array_push($data, $new_values);
-//}
-setcookie('submissions', json_encode($values), time()+3600);
-    if(isset($_COOKIE['submissions'])) $data[] = $_COOKIE['submissions'];
+?>
 
-    print_r($data);
-    ?>
-
-    <?php if($data): ?>
-        <table>
-            <?php foreach($data as $submission):?>
-                <td><?php echo $submission['name']; ?></td>
-                <td><?php echo $submission['email']; ?></td>
-                <td><?php echo $submission['message']; ?></td>
+<?php if($data): ?>
+    <table>
+        <?php foreach($data as $submission):?>
+        <tr>
+            <td><?php echo $submission['name']; ?></td>
+            <td><?php echo $submission['email']; ?></td>
+            <td><?php echo $submission['message']; ?></td>
+        <tr>
             <?php endforeach; ?>
-        </table>
-    <?php endif; ?>
+    </table>
+<?php endif; ?>
 
-    </body>
-    </html>
-
-
-
+</body>
+</html>
